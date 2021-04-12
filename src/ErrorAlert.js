31f95ada32eab07283666ce2d03e14147cad2444
Mackenzie Howey <Howeymackenzie@gmail.com>
2021-04-12 14:27:45 -0400

    added error handling  :skull:  :guardsman:  :no_bell:

---
+++ src/ErrorAlert.js
@@ -0,0 +1,10 @@
+import "./errorAlert.css";
+function ErrorAlert() {
+  return (
+    <div className='errorPopUp hide'>
+      <h3>Please Enter A Valid Movie Title</h3>
+    </div>
+  );
+}
+
+export default ErrorAlert;
--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,38 +1,59 @@
-import './searchResult.css';
+import "./searchResult.css";
 
 function SearchResult(props) {
-    return (
-        <>
-            <div className="resultContainer">
-                <div className="poster">
-                    <img src={props.poster} alt={props.title}/>
-                </div>
-                <ul>
-                    <li>
-                        <p><span className="info">Title: </span>{props.title}</p>
-                    </li>
-                    <li>
-                        <p><span className="info">Release Date: </span>{props.released}</p>
-                    </li>
-                    <li>
-                        <p><span className="info">Runtime: </span>{props.runtime}</p>
-                    </li>
-                    <li>
-                        <p><span className="info">Rated: </span>{props.rated}</p>
-                    </li>
-                    <li>
-                        <p><span className="info">Genre: </span>{props.genre}</p>
-                    </li>
-                    <li>
-                        <p><span className="info">Plot: </span>{props.plot}</p>
-                    </li>
-                    <li>
-                        <p><span className="info">Actors: </span>{props.actors}</p>
-                    </li>
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className='resultContainer'>
+        <div className='poster'>
+          <img src={props.poster} alt={props.title} />
+        </div>
+        <ul>
+          <li>
+            <p>
+              <span className='info'>Title: </span>
+              {props.title}
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className='info'>Release Date: </span>
+              {props.released}
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className='info'>Runtime: </span>
+              {props.runtime}
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className='info'>Rated: </span>
+              {props.rated}
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className='info'>Genre: </span>
+              {props.genre}
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className='info'>Plot: </span>
+              {props.plot}
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className='info'>Actors: </span>
+              {props.actors}
+            </p>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default SearchResult;
