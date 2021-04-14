f907318f67d18e4a2311abf5fd74e911e46bad3a
Mackenzie Howey <Howeymackenzie@gmail.com>
2021-04-14 14:35:30 -0400

    finalized responsiveness and fixed minor bugs :star::rocket::fire:

--- src/PlanToWatchList.js
+++ src/PlanToWatchList.js
@@ -1,23 +1,25 @@
-import './planToWatchList.css';
+import "./planToWatchList.css";
 
 function PlanToWatchList(props) {
-    return(
-        <>
-            <li key={props.key} className="movieCard">
-                <div className="cardInfo">
-                    <div className="posterContainer">
-                        <img src={props.poster} alt={props.title}/>
-                        <div className="overlay" tabindex="0">
-                            <p>{props.title}</p>
-                            <p>{props.released}</p>
-                            <p>{props.genre}</p>
-                        </div>
-                    </div>
-                </div>
-                <button onClick={props.removeMovie}><i className="fas fa-trash-alt" aria-label="remove movie"></i></button>
-            </li>
-        </>
-    )
+  return (
+    <>
+      <li key={props.key} className='movieCard'>
+        <div className='cardInfo'>
+          <div className='posterContainer'>
+            <img src={props.poster} alt={props.title} />
+            <div className='overlay' tabIndex='0'>
+              <p>{props.title}</p>
+              <p>{props.released}</p>
+              <p>{props.genre}</p>
+            </div>
+          </div>
+        </div>
+        <button onClick={props.removeMovie}>
+          <i className='fas fa-trash-alt' aria-label='remove movie'></i>
+        </button>
+      </li>
+    </>
+  );
 }
 
 export default PlanToWatchList;
